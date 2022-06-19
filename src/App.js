c023726104d7c0b901cd23583d3ab5b1201a0ae3
RaianeMarcelino <104740042+RaianeMarcelino@users.noreply.github.com>
2022-06-19 12:53:24 -0300

    Revert "Raiane marcelino carrinho"

--- src/App.js
+++ src/App.js
@@ -4,38 +4,6 @@ import produtosList from "./Components/data/produtos.json"
 import Filtros from './Components/Filtros';
 import { Home } from './Components/Home'
 
- export class App extends Component {
-state = {
-    produtos: produtosList
-   }
-
-   render() {
-      return (
-     <div>
-
-    <h3> Quantidade de produtos:{this.state.produtos.length} </h3>
-          {this.state.produtos.map(produto => {
-return <Home key = {produto.key} produto={produto} />
-          })}
-      <Filtros></Filtros>
-      
-      </div>
-     )
-  }
-}
-
-
-export  class Carrinho extends Component {
-  state={
-    carrinho = []
-  }
-  valorTotal=() => {
-    produtosList.preco + produtosList.preco
-  }
-  render() {
-    return (
-      <div><h1>Carrinho</h1>
-
 export class App extends Component {
   state = {
     produtos: produtosList
@@ -48,10 +16,8 @@ export class App extends Component {
             return <Home key = {produto.key} produto={produto} />
           })}
         <Filtros></Filtros>
-
       </div>
      )
   }
 }
-
 export default App;
